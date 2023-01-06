1ecf1d1adf9874d0ba7e54acb206874e10c33313
unknown <k.jones45@yahoo.com>
2023-01-06 00:38:25 -0500

    Put Battlefield Class on battlefield.py. Added method functions.

---
+++ battlefield.py
@@ -0,0 +1,13 @@
+class Battlefield:
+    
+    def __init__(self):
+        pass
+
+    run_game(self):
+    pass
+    display_welcome(self):
+    pass
+
+    battle_phase(self):
+    pass
+    display_winner(self):
