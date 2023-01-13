145b2f9ac6d4fee2f3035144ae019ce5054ee44f
unknown <k.jones45@yahoo.com>
2023-01-13 14:46:40 -0500

    Working on battlephase method

--- battlefield.py
+++ battlefield.py
@@ -6,12 +6,34 @@ class Battlefield:
         self.robot = Robot("R2",50)
         self.dino = Dinosaur("Kesha",50,100)
         pass
+
+    #Master Method
     def run_game(self):
+        self.display_welcome()
+        self.battle_phase()
+        self.display_winner()
         pass
     
     def display_welcome(self):
         print("Welcome to the showdown!")
+
     def battle_phase(self):
-        pass
+        while self.robot.health < 50:
+         if self.dino.attack(self.robot): 
+            print("R2 attacks Kesha!")
+
+         elif self.dino.health < 100:
+            self.robot.attack(self.dino) 
+            print("Kesha attacks R2!")
+            break
+        
+    #  while (self.robot.health > 0 or self.dino.health > 0):
+ 
     def display_winner(self):
+       if self.robot.health <= 0:
+        print("Game Concluded: The winner is Ke$ha!")
+       else:
+        print("Game Concluded: The winner is R2!")
+
+         
         pass
